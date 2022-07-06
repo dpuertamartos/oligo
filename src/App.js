eada283f26eb4f7d1f9fe95510f25ae6261b6b22
David Puerta Martos <dpuerta@usal.es>
2022-07-07 01:11:18 +0200

    OLIGO FILTER V0 INCLUDED

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [oligos, setOligos] = useState([])
+  const [oligos, setOligos] = useState([
+    { id: 1, sequence: "ATTAGC" },
+    { id: 2, sequence: "GCGCAA"  },
+    { id: 3, sequence: "TTAAGG"  },
+    { id: 4, sequence: "CCATGG"  }
+  ])
   const [newOligo, setNewOligo] = useState("Enter oligonucleotid")
+  const [showAll, setShowAll] = useState(true)
+  const [newSearch, setNewSearch] = useState("Search oligo")
 
   const addOligo = (event) => {
     event.preventDefault()
@@ -18,14 +25,27 @@ const App = () => {
   
   const handleOligoChange = (event) => {
     console.log(event.target.value)
-    setNewOligo(event.target.value)
+    setNewOligo(event.target.value.toUpperCase())
   }
+
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    setNewSearch(event.target.value.toUpperCase())
+  }
+
+  const oligosToShow = showAll
+    ? oligos
+    : oligos.filter(oligo=>oligo.sequence.includes(newSearch) )
 
   return (
     <div>
       <h1>Oligos</h1>
+      <input value={newSearch} onChange={handleSearchChange}/>
+      <button onClick={() => setShowAll(!showAll)}>
+        {showAll ? 'filter mode' : 'show all'}
+      </button>
       <ul>
-        {oligos.map(oligo => 
+        {oligosToShow.map(oligo => 
           <li key={oligo.id}>{oligo.sequence}</li>
         )}
       </ul>
