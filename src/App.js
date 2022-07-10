ccb5186b6d11be997e3c705b569f7268e968bb65
David Puerta Martos <dpuerta@usal.es>
2022-07-10 20:15:42 +0200

    state of oligos, oligoform and access to oligoservice logic moved from app to oligos component

--- src/App.js
+++ src/App.js
@@ -4,30 +4,18 @@ import loginService from './services/login'
 import Oligos from './components/Oligos'
 import Genes from './components/Genes'
 import Plasmids from './components/Plasmids'
-import Filter from './components/Filter'
-import AddOligoForm from './components/AddOligoForm'
 import LoginForm from './components/LoginForm'
 import Togglable from './components/Togglable'
 import Notification from './components/Notification'
 
 
 const App = () => {
-  const [oligos, setOligos] = useState([])
-  const [showAll, setShowAll] = useState(true)
-  const [newSearch, setNewSearch] = useState("Search sequence")
+  
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   
-  useEffect(() => {
-    oligoService
-      .getAll()
-      .then(initialOligos => {
-        setOligos(initialOligos)
-      })
-  }, [])
-
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
     if (loggedUserJSON) {
@@ -36,71 +24,6 @@ const App = () => {
       oligoService.setToken(user.token)
     }
   }, [])
-
-  const addOligo = (oligoObject) => {
-    oligoFormRef.current.toggleVisibility()
-    oligoService
-      .create(oligoObject)
-      .then(returnedOligo => {
-        setOligos(oligos.concat(returnedOligo))
-        setErrorMessage(
-          [`oligo ${oligoObject.sequence} was added to server`,"confirmation"]
-        )
-        setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-      })
-  }
-
-  const updateOligo = (id, newsequence) => {
-    const oligo = oligos.find(o => o.id === id)
-    const changedOligo = {...oligo, sequence: newsequence}
-
-    oligoService
-      .update(id, changedOligo)
-      .then(returnedOligo =>{
-        setOligos(oligos.map(oligo => oligo.id !== id ? oligo : returnedOligo))
-      })
-      .catch(error => {
-        setErrorMessage(
-          [`oligo ${oligo.sequence} was already deleted from server`,"error"]
-        )
-        setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-        setOligos(oligos.filter(n=>n.id !== id))
-      })
-  }
-
-  const deleteOligo = id => {
-    const oligo = oligos.find(o => o.id === id)
-    if(window.confirm(`do you want delete oligo with ID: ${id} and sequence: ${oligo.sequence}?`)){
-      oligoService
-        .remove(id)
-        .then(()=>{
-          setOligos(oligos.filter(n => n.id !== id))
-          setErrorMessage(
-            [`oligo ${oligo.sequence} was deleted from server`,"confirmation"]
-          )
-        })
-        .catch(error => {
-          setErrorMessage(
-            [`oligo ${oligo.sequence} was already deleted from server`,"error"]
-          )
-          setTimeout(() => {
-            setErrorMessage(null)
-          }, 5000)
-          setOligos(oligos.filter(n=>n.id !== id))
-        })
-    }
-  }
-  
-  
-
-  const handleSearchChange = (event) => {
-    console.log(event.target.value)
-    setNewSearch(event.target.value.toUpperCase())
-  }
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -124,15 +47,11 @@ const App = () => {
     }
   }
 
-  const oligosToShow = showAll
-    ? oligos
-    : oligos.filter(oligo=>oligo.sequence.includes(newSearch) )
-
-  const oligoFormRef = useRef()
+  
 
   return (
     <div>
-      <h1>Oligos</h1>
+      {user !== null && <div>{user.name} logged-in</div>}
       <Notification message={errorMessage} />
       {user === null && 
       <Togglable buttonLabel='login'>
@@ -145,23 +64,12 @@ const App = () => {
         />
       </Togglable>
       }
-      <Filter ipValue={newSearch} ipOnChange={handleSearchChange}/>
-      <button onClick={() => setShowAll(!showAll)}>
-        {showAll ? 'filter OFF, click to activate' : 'FILTER ON, click to deactivate'}
-      </button>
-      <Oligos oligosToShow = {oligosToShow} editOligo ={updateOligo} deleteOligo = {deleteOligo} />
+      <h1>Oligos</h1>
+      <Oligos setErrorMessage={setErrorMessage} user={user}/>
       <h1>Genes</h1>
       <Genes />
       <h1>Plasmids</h1>
-      <Plasmids />
-      {user !== null &&
-      <Togglable buttonLabel="new oligo" ref={oligoFormRef}>
-        <AddOligoForm
-          createOligo={addOligo}
-        />
-      </Togglable>
-      }
-      {user !== null && <div>{user.name} logged-in</div>}
+      <Plasmids /> 
     </div>
   )
 }
--- src/components/Oligos.js
+++ src/components/Oligos.js
@@ -1,13 +1,112 @@
 import React from 'react'
+import {useState, useEffect, useRef} from 'react'
+import oligoService from '../services/oligos'
 import Oligo from './Oligo'
+import Togglable from './Togglable'
+import AddOligoForm from './AddOligoForm'
+import Filter from './Filter'
 
-const Oligos = ({oligosToShow, editOligo, deleteOligo}) => {
+const Oligos = ({setErrorMessage, user}) => {
+    const [oligos, setOligos] = useState([])
+    const [showAll, setShowAll] = useState(true)
+    const [newSearch, setNewSearch] = useState("Search sequence")
+
+    useEffect(() => {
+        oligoService
+          .getAll()
+          .then(initialOligos => {
+            setOligos(initialOligos)
+          })
+      }, [])
+    
+    const addOligo = (oligoObject) => {
+    oligoFormRef.current.toggleVisibility()
+    oligoService
+        .create(oligoObject)
+        .then(returnedOligo => {
+        setOligos(oligos.concat(returnedOligo))
+        setErrorMessage(
+            [`oligo ${oligoObject.sequence} was added to server`,"confirmation"]
+        )
+        setTimeout(() => {
+            setErrorMessage(null)
+        }, 5000)
+        })
+    }
+
+    const updateOligo = (id, newsequence) => {
+        const oligo = oligos.find(o => o.id === id)
+        const changedOligo = {...oligo, sequence: newsequence}
+    
+        oligoService
+          .update(id, changedOligo)
+          .then(returnedOligo =>{
+            setOligos(oligos.map(oligo => oligo.id !== id ? oligo : returnedOligo))
+          })
+          .catch(error => {
+            setErrorMessage(
+              [`oligo ${oligo.sequence} was already deleted from server`,"error"]
+            )
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+            setOligos(oligos.filter(n=>n.id !== id))
+          })
+      }
+    
+      const deleteOligo = id => {
+        const oligo = oligos.find(o => o.id === id)
+        if(window.confirm(`do you want delete oligo with ID: ${id} and sequence: ${oligo.sequence}?`)){
+          oligoService
+            .remove(id)
+            .then(()=>{
+              setOligos(oligos.filter(n => n.id !== id))
+              setErrorMessage(
+                [`oligo ${oligo.sequence} was deleted from server`,"confirmation"]
+              )
+            })
+            .catch(error => {
+              setErrorMessage(
+                [`oligo ${oligo.sequence} was already deleted from server`,"error"]
+              )
+              setTimeout(() => {
+                setErrorMessage(null)
+              }, 5000)
+              setOligos(oligos.filter(n=>n.id !== id))
+            })
+        }
+      }
+    
+    const oligosToShow = showAll
+      ? oligos
+      : oligos.filter(oligo=>oligo.sequence.includes(newSearch) )
+  
+    const oligoFormRef = useRef()
+
+    const handleSearchChange = (event) => {
+        console.log(event.target.value)
+        setNewSearch(event.target.value.toUpperCase())
+      }
+
     return (
+        <div>
+        <Filter ipValue={newSearch} ipOnChange={handleSearchChange}/>
+        <button onClick={() => setShowAll(!showAll)}>
+            {showAll ? 'filter OFF, click to activate' : 'FILTER ON, click to deactivate'}
+        </button>
         <ul>
             {oligosToShow.map(oligo => 
-            <Oligo key={oligo.id} oligo={oligo} editOligo={editOligo} deleteOligo={deleteOligo} />
+            <Oligo key={oligo.id} oligo={oligo} editOligo={updateOligo} deleteOligo={deleteOligo} />
             )}
         </ul>
+        {user !== null &&
+        <Togglable buttonLabel="new oligo" ref={oligoFormRef}>
+            <AddOligoForm
+            createOligo={addOligo}
+            />
+        </Togglable>
+        }
+        </div>
     )}
 
 export default Oligos
