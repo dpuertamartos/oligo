7d4e81667916f2a2115d87ae5c78376c4a7aaa13
David Puerta Martos <dpuerta@usal.es>
2022-07-18 17:29:49 +0200

    filter state moved to redux

--- src/components/Oligos.js
+++ src/components/Oligos.js
@@ -6,16 +6,13 @@ import AddOligoForm from './AddOligoForm'
 import Filter from './Filter'
 import filter from '../logic/filter'
 import { createOligo, removeOligo, editOligo } from '../reducers/oligoReducer'
-import { createNotification } from '../reducers/notificationReducer'
+import { appendType, deleteType, changeSearch } from '../reducers/filterReducer'
 import { useSelector, useDispatch } from 'react-redux'
 
 const Oligos = ({user}) => {
     const dispatch = useDispatch()
     const oligos = useSelector(state => state.oligos)
-    
-    const [newSearch, setNewSearch] = useState(["Search sequence","Search gene","Search plasmid"])
-    const [filterType, setFilterType] = useState([])
-
+    const filterstate = useSelector(state => state.filter)
     
     const addOligo = (oligoObject) => {
       oligoFormRef.current.toggleVisibility()
@@ -39,45 +36,43 @@ const Oligos = ({user}) => {
     }
     
     const handleFilterChange = (key) => {
-      if(filterType.includes(key)){
-        setFilterType(filterType.filter(k => k !== key))
+      if(filterstate.type.includes(key)){
+        dispatch(deleteType(key))
       }
       else{
-        setFilterType(filterType.concat(key))
+        dispatch(appendType(key))
       }
-      console.log(filterType)
+      console.log(filterstate)
     }
     
     const oligoFormRef = useRef()
 
     const handleSearchChange1 = (event) => {
         console.log(event.target.value)
-        setNewSearch(newSearch.map((item,index) => {
-          if(index!==0){return item}
-          else{return event.target.value.toUpperCase()}
-        }))
-        console.log(newSearch)
+        dispatch(changeSearch({pos:0,change:event.target.value.toUpperCase()}
+
+        ))
+        console.log(filterstate.search)
       }
     
       const handleSearchChange2 = (event) => {
         console.log(event.target.value)
-        setNewSearch(newSearch.map((item,index) => {
-          if(index!==1){return item}
-          else{return event.target.value.toUpperCase()}
-        }))
-        console.log(newSearch)
+        dispatch(changeSearch({pos:1,change:event.target.value.toUpperCase()}
+
+        ))
+        console.log(filterstate.search)
     }
     
     const handleSearchChange3 = (event) => {
       console.log(event.target.value)
-      setNewSearch(newSearch.map((item,index) => {
-        if(index!==2){return item}
-        else{return event.target.value.toUpperCase()}
-      }))
-      console.log(newSearch)
+      dispatch(changeSearch({pos:2,change:event.target.value.toUpperCase()}
+
+      ))
+      console.log(filterstate.search)
+      
     }
 
-    const oligosToShow = filter(oligos, filterType, newSearch)
+    const oligosToShow = filter(oligos, filterstate.type, filterstate.search)
 
     return (
         <div>
@@ -95,31 +90,31 @@ const Oligos = ({user}) => {
         <div className="container-fluid filterForm">
           <div className ="row">
               <div className="col">
-              <Filter ipValue={newSearch[0]} ipOnChange={handleSearchChange1}/>
+              <Filter ipValue={filterstate.search[0]} ipOnChange={handleSearchChange1}/>
               </div>
               <div className="col d-grid gap-2">
-              <button type="button" className={`btn${filterType.includes("sequence") ? "" : "-outline"}-success`} onClick={() => handleFilterChange("sequence")}>
-                  {filterType.includes("sequence") ? 'seq filter ON' : 'seq filter OFF'}
+              <button type="button" className={`btn${filterstate.type.includes("sequence") ? "" : "-outline"}-success`} onClick={() => handleFilterChange("sequence")}>
+                  {filterstate.type.includes("sequence") ? 'seq filter ON' : 'seq filter OFF'}
               </button>   
               </div>
           </div>
           <div className ="row">
             <div className="col">
-            <Filter ipValue={newSearch[1]} ipOnChange={handleSearchChange2}/>  
+            <Filter ipValue={filterstate.search[1]} ipOnChange={handleSearchChange2}/>  
             </div>
             <div className="col d-grid gap-2">
-            <button type="button" className={`btn${filterType.includes("gene") ? "" : "-outline"}-success`} onClick={() => handleFilterChange("gene")}>
-                {filterType.includes("gene") ? 'gene filter ON' : 'gene filter OFF'}
+            <button type="button" className={`btn${filterstate.type.includes("gene") ? "" : "-outline"}-success`} onClick={() => handleFilterChange("gene")}>
+                {filterstate.type.includes("gene") ? 'gene filter ON' : 'gene filter OFF'}
             </button>  
             </div>
           </div>
           <div className ="row">
             <div className="col">
-              <Filter ipValue={newSearch[2]} ipOnChange={handleSearchChange3}/>
+              <Filter ipValue={filterstate.search[2]} ipOnChange={handleSearchChange3}/>
             </div>
             <div className="col d-grid gap-2">
-              <button type="button" className={`btn${filterType.includes("plasmid") ? "" : "-outline"}-success`} onClick={() => handleFilterChange("plasmid")}>
-                {filterType.includes("plasmid") ? 'plasmid filter ON' : 'plasmid filter OFF'}
+              <button type="button" className={`btn${filterstate.type.includes("plasmid") ? "" : "-outline"}-success`} onClick={() => handleFilterChange("plasmid")}>
+                {filterstate.type.includes("plasmid") ? 'plasmid filter ON' : 'plasmid filter OFF'}
               </button>
             </div>
           </div>
